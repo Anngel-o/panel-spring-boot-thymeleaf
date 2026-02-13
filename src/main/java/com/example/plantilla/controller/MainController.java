0140548589fe679033a172b63682564f9ee3b8e5
Ángel Ortiz <anngelo.omz@gmail.com>
2026-02-12 21:46:42 -0600

    create dynamic list

--- src/main/java/com/example/plantilla/controller/MainController.java
+++ src/main/java/com/example/plantilla/controller/MainController.java
@@ -22,11 +22,30 @@ public class MainController {
         panelDefault.add(new PanelObject("Pokemon 2", "Pikachu"));
         panelDefault.add(new PanelObject("Pokemon 3", "Monkey"));
         panelDefault.add(new PanelObject("Pokemon 4", "Hitmonlee"));
-        panelDefault.add(new PanelObject("Pokemon 5", ""));
+        panelDefault.add(new PanelObject("Pokemon 5", "Pichu"));
+        panelDefault.add(new PanelObject("Pokemon 6", "Chansey"));
+        panelDefault.add(new PanelObject("Pokemon 7", "Pidgotto"));
+        panelDefault.add(new PanelObject("Pokemon 8", "Blastoise"));
+        panelDefault.add(new PanelObject("Pokemon 9", "Alakasam"));
+        panelDefault.add(new PanelObject("Pokemon 10", "Ditto"));
 
-
+        List<PanelObject> panelPrimary = new ArrayList<>();
+        panelPrimary.add(new PanelObject("Nombre", "Angel Ortiz"));
+        panelPrimary.add(new PanelObject("Correo", "anngelo.omz@gmail.com"));
+        panelPrimary.add(new PanelObject("Github:", "https://github.com/anngel-o"));
+        panelPrimary.add(new PanelObject("Pokemon 1", "Charizard"));
+        panelPrimary.add(new PanelObject("Pokemon 2", "Pikachu"));
+        panelPrimary.add(new PanelObject("Pokemon 3", "Monkey"));
+        panelPrimary.add(new PanelObject("Pokemon 4", "Hitmonlee"));
+        panelPrimary.add(new PanelObject("Pokemon 5", "Pichu"));
+        panelPrimary.add(new PanelObject("Pokemon 6", "Chansey"));
+        panelPrimary.add(new PanelObject("Pokemon 7", "Pidgotto"));
+        panelPrimary.add(new PanelObject("Pokemon 8", "Blastoise"));
+        panelPrimary.add(new PanelObject("Pokemon 9", "Alakasam"));
+        panelPrimary.add(new PanelObject("Pokemon 10", "Ditto"));
 
         model.addAttribute("panelDefault", panelDefault);
+        model.addAttribute("panelPrimary", panelPrimary);
 
         return "index";
     }
